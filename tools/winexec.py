8d93d77a316f5156b83529ef761c4dbcc9d12a93
skywind3000 <skywind3000@163.com>
2016-04-06 16:45:55 +0800

    new windows open

--- tools/winexec.py
+++ tools/winexec.py
@@ -11,7 +11,11 @@ class configure (object):
 	def __init__ (self):
 		self.dirhome = os.path.abspath(os.path.dirname(__file__))
 		self.diruser = os.path.abspath(os.path.expanduser('~'))
-		self.win32 = sys.platform[:3] == 'win' and True or False
+		self.unix = sys.platform[:3] != 'win' and True or False
+		self.temp = os.environ.get('temp', os.environ.get('tmp', '/'))
+		self.tick = long(time.time()) % 100
+		self.temp = os.path.join(self.temp, 'winex_%02d.cmd'%self.tick)
+		self.GetShortPathName = None
 	
 	def escape (self, path):
 		path = path.replace('\\', '\\\\').replace('"', '\\"')
@@ -70,9 +74,70 @@ class configure (object):
 		osascript.append('end tell')
 		osascript.append('end tell')
 		return self.darwin_osascript(osascript)
+	
+	def win32_escape (self, argument, force = False):
+		if force == False and argument:
+			clear = True
+			for n in ' \n\r\t\v\"':
+				if n in argument:
+					clear = False
+					break
+			if clear:
+				return argument
+		output = '"'
+		size = len(argument)
+		i = 0
+		while True:
+			blackslashes = 0
+			while (i < size and argument[i] == '\\'):
+				i += 1
+				blackslashes += 1
+			if i == size:
+				output += '\\' * (blackslashes * 2)
+				break
+			if argument[i] == '"':
+				output += '\\' * (blackslashes * 2 + 1)
+				output += '"'
+			else:
+				output += '\\' * blackslashes
+				output += argument[i]
+			i += 1
+		output += '"'
+		return output
+
+	def win32_path_short (self, path):
+		path = os.path.abspath(path)
+		if self.unix:
+			return path
+		if not self.GetShortPathName:
+			self.kernel32 = None
+			self.textdata = None
+			try:
+				import ctypes
+				self.kernel32 = ctypes.windll.LoadLibrary("kernel32.dll")
+				self.textdata = ctypes.create_string_buffer('\000' * 1024)
+				self.GetShortPathName = self.kernel32.GetShortPathNameA
+				args = [ ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int ]
+				self.GetShortPathName.argtypes = args
+				self.GetShortPathName.restype = ctypes.c_uint32
+			except: pass
+		if not self.GetShortPathName:
+			return path
+		retval = self.GetShortPathName(path, self.textdata, 1024)
+		shortpath = self.textdata.value
+		if retval <= 0:
+			return ''
+		return shortpath
 
 	def win32_open_console (self, title, script):
-
+		fp = open(self.temp, 'w')
+		fp.write('@echo off\n')
+		for line in script:
+			fp.write(line + '\n')
+		fp.close()
+		fp = None
+		pathname = self.win32_path_short(self.temp)
+		os.system('start "%s" cmd /C %s'%(title, pathname))
 		return 0
 
 
@@ -88,7 +153,12 @@ if __name__ == '__main__':
 		cfg = configure()
 		cfg.darwin_open_iterm2('11111', ['sleep 2', 'read -n1 -rsp press\\ any\\ key\\ to\\ continue...', 'echo "fuck you"', 'sleep 10'])
 
-	test2()
+	def test3():
+		cfg = configure()
+		cfg.win32_open_console('11111', ['d:', 'cd /acm/github/vim/tools', 'dir', 'pause' ])
+		return 0
+
+	test3()
 
 
 
