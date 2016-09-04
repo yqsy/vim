f42e9808c8a5ab0aa6b04df354d4cd31997ea56f
skywind3000 <skywind3000@163.com>
2016-09-04 16:02:30 +0800

    new commands

---
+++ tools/win32/vimmake.5.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+if "%VIM_FILENAME%" == "" GOTO ERROR_NO_FILE
+
+REM CD /D "%VIM_FILEDIR%"
+
+d:\dev\mingw\bin\gcc -Wall -mwindows -O3 "%VIM_FILEPATH%" -o "%VIM_FILEDIR%/%VIM_FILENOEXT%" -lwinmm -lstdc++ -lgdi32 -lws2_32 -msse3
+
+GOTO END
+
+
+:ERROR_NO_FILE
+echo missing file name
+
+:END
+
+
---
+++ tools/win32/vimmake.6.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+CD /D "%VIM_FILEDIR%"
+d:\dev\pypy\pypy2-v5.4.0-win32\pypy.exe "%VIM_FILENAME%"
+
+
