c271608d9c80135aa60c2be6166ab710843955e9
skywind3000 <skywind3000@163.com>
2016-09-04 16:11:16 +0800

    modify vimmake.5

--- tools/win32/vimmake.5.cmd
+++ tools/win32/vimmake.5.cmd
@@ -1,17 +1,6 @@
 @ECHO OFF
 
-if "%VIM_FILENAME%" == "" GOTO ERROR_NO_FILE
-
-REM CD /D "%VIM_FILEDIR%"
-
-d:\dev\mingw\bin\gcc -Wall -mwindows -O3 "%VIM_FILEPATH%" -o "%VIM_FILEDIR%/%VIM_FILENOEXT%" -lwinmm -lstdc++ -lgdi32 -lws2_32 -msse3
-
-GOTO END
-
-
-:ERROR_NO_FILE
-echo missing file name
-
-:END
+CD /D "%VIM_FILEDIR%"
+d:\dev\pypy\pypy2-v5.4.0-win32\pypy.exe "%VIM_FILENAME%"
 
 
