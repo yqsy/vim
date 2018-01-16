2f0eab76158d4a5231c39f8c4eaf8a50f35736cb
skywind3000 <skywind3000@163.com>
2018-01-17 00:36:30 +0800

    commit new fortune

--- login/02-post.sh
+++ login/02-post.sh
@@ -3,8 +3,9 @@ FORTUNES=/usr/share/games/fortunes
 
 if [ -x $FORTUNE ]; then
 	if [ -f $FORTUNES/debian-hints ]; then echo ""; $FORTUNE debian-hints; fi
+	if [ -f $FORTUNES/wisdom ]; then echo ""; $FORTUNE wisdom; fi
 	# if [ -f $FORTUNES/linux ]; then echo ""; fortune linux; fi
-	echo ""; $FORTUNE; echo ""
+	echo ""
 fi
 
 
