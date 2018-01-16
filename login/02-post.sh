220a00ea6d8658015f6a72c0797dae7939dd4bcc
skywind3000 <skywind3000@163.com>
2018-01-16 23:47:45 +0800

    update fortune

--- login/02-post.sh
+++ login/02-post.sh
@@ -1,9 +1,10 @@
+FORTUNE=/usr/games/fortune
 FORTUNES=/usr/share/games/fortunes
 
-if [ -x /usr/games/fortune ]; then
-	if [ -f $FORTUNES/debian-hints ]; then echo ""; fortune debian-hints; fi
+if [ -x $FORTUNE ]; then
+	if [ -f $FORTUNES/debian-hints ]; then echo ""; $FORTUNE debian-hints; fi
 	# if [ -f $FORTUNES/linux ]; then echo ""; fortune linux; fi
-	echo ""; fortune; echo ""
+	echo ""; $FORTUNE; echo ""
 fi
 
 
