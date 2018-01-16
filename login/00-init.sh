f689f982c92a3aa06ad1b72bb99d31d4cb6950ff
skywind3000 <skywind3000@163.com>
2018-01-17 01:05:31 +0800

    commit new init script

--- login/00-init.sh
+++ login/00-init.sh
@@ -5,8 +5,7 @@ export LANG=en_US.UTF-8
 
 export DEFAULT_CHEAT_DIR=~/.vim/vim/cheat
 export GOPATH="$HOME/.local/go"
-
-export PATH="/usr/local/app/node-v8.9.4-linux-x64/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
 
 
 gdbtool () { emacs --eval "(gdb \"gdb -i=mi $*\")"; }
