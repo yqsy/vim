7cb8bbbf2997eabe29d60868c6e754257f92f39a
skywind3000 <skywind3000@163.com>
2018-01-19 03:16:45 +0800

    update config

--- etc/config.sh
+++ etc/config.sh
@@ -5,7 +5,10 @@ alias la='ls -lAh'
 alias grep='grep --color=tty'
 alias nvim='/usr/local/opt/bin/vim'
 alias mvim='/usr/local/opt/bin/vim --cmd "let g:vim_startup=\"mvim\""'
+# alias tmux='tmux -2'
 
+# term color
+export TERM=xterm-256color
 
 # setup for go if it exists
 if [ -d "$HOME/.local/go" ]; then
@@ -30,6 +33,21 @@ fi
 if [ -d "$HOME/.vim/vim/cheat" ]; then
 	export DEFAULT_CHEAT_DIR=~/.vim/vim/cheat
 fi
+
+listcolor() {
+	# for i in {0..255}; do
+	# 	printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+	# done;
+	for i in {0..255}; do 
+		printf "\x1b[38;5;${i}mcolour%-5i\x1b[0m" $i 
+		if ! (( ($i + 1 ) % 8 )); then 
+			echo ""
+		fi 
+	done
+}
+
+gdbtool () { emacs --eval "(gdb \"gdb --annotate=3 -i=mi $*\")";}
+
 
 
 
