de68f1356130301fe150f9c546c6ef36015461f9
skywind3000 <skywind3000@163.com>
2018-01-20 18:56:50 +0800

    update zsh setup

--- etc/setup.zsh
+++ etc/setup.zsh
@@ -1,34 +1,24 @@
-[ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2> /dev/null
-[ ! -d "$HOME/.local/zsh" ] && mkdir -p "$HOME/.local/zsh" 2> /dev/null
-
-
 if [ ! -d "$HOME/.local/zsh/antigen" ]; then
 	echo "Installing antigen ..."
+	[ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2> /dev/null
+	[ ! -d "$HOME/.local/zsh" ] && mkdir -p "$HOME/.local/zsh" 2> /dev/null
+	[ ! -f "$HOME/.z" ] && touch "$HOME/.z"
 	git clone https://github.com/zsh-users/antigen.git "$HOME/.local/zsh/antigen" 
 fi
 
 ANTIGEN="$HOME/.local/zsh/antigen"
-
-if [ ! -d "$ANTIGEN" ]; then
-	echo "can not install antigen, check $0 please"
-	exit
-fi
 
 if [ ! -f "$ANTIGEN/antigen.zsh" ]; then
 	echo "can not find antigen, check $0 please"
 	exit
 fi
 
-if [ ! -f "$HOME/.local/etc/init.sh" ]; then
-	echo "can not find ~/.local/etc/init.sh, check $0 please"
-	exit
-fi
+export PS1="$ "
+export PS1="%n@%m:%~%# "
 
 source "$ANTIGEN/antigen.zsh"
 [ -f "$HOME/.local/etc/init.sh" ] && source "$HOME/.local/etc/init.sh"
 [ -f "$HOME/.local/etc/config.zsh" ] && source "$HOME/.local/etc/config.zsh" 
-
-export PS1="$ "
 
 antigen use oh-my-zsh
 
