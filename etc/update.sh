b3dcc93dec6c746cb0d9771a41a406583ee8e69f
skywind3000 <skywind3000@163.com>
2018-01-20 16:21:45 +0800

    update fish config

--- etc/update.sh
+++ etc/update.sh
@@ -16,8 +16,18 @@ if [ ! -d "$HOME/.local/bin" ]; then
 	mkdir -p "$HOME/.local/bin" 2> /dev/null
 fi
 
+if [ ! -d "$HOME/.config" ]; then
+	mkdir -p "$HOME/.config" 2> /dev/null
+fi
+
+if [ ! -d "$HOME/.config/fish" ]; then
+	mkdir -; "$HOME/.config/fish" 2> /dev/null
+fi
+
+
 cp $SCRIPTPATH/*.sh "$HOME/.local/etc/" 
 cp $SCRIPTPATH/*.conf "$HOME/.local/etc/" 
+cp $SCRIPTPATH/*.fish "$HOME/.config/fish"
 
 cp $SCRIPTPATH/../tools/bin/* "$HOME/.local/bin"
 
