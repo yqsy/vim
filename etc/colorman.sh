7879477c0bb09c555771bceab080a93b14fb47c8
skywind3000 <skywind3000@163.com>
2018-01-21 03:45:43 +0800

    update xterm fast color

--- etc/colorman.sh
+++ etc/colorman.sh
@@ -15,18 +15,18 @@
 # export LESS_TERMCAP_ZW=$(tput rsupm)
 
 # new: directly color escape code, much faster
-export LESS_TERMCAP_mh="\033[2m"
-export LESS_TERMCAP_me="\033(B\033[m"
-export LESS_TERMCAP_md="\033[1m\033[36m"
-export LESS_TERMCAP_mb="\033[1m\033[32m"
-export LESS_TERMCAP_ue="\033[24m\033(B\033[m"
-export LESS_TERMCAP_ZN=""
-export LESS_TERMCAP_ZO=""
-export LESS_TERMCAP_mr="\033[7m"
-export LESS_TERMCAP_se="\033[27m\033(B\033[m"
-export LESS_TERMCAP_ZV=""
-export LESS_TERMCAP_us="\033[4m\033[1m\033[37m"
-export LESS_TERMCAP_so="\033[1m\033[33m\033[44m"
+export LESS_TERMCAP_mb=$'\E[1m\E[32m'
+export LESS_TERMCAP_mh=$'\E[2m'
+export LESS_TERMCAP_mr=$'\E[7m'
+export LESS_TERMCAP_md=$'\E[1m\E[36m'
 export LESS_TERMCAP_ZW=""
+export LESS_TERMCAP_us=$'\E[4m\E[1m\E[37m'
+export LESS_TERMCAP_me=$'\E(B\E[m'
+export LESS_TERMCAP_ue=$'\E[24m\E(B\E[m'
+export LESS_TERMCAP_ZO=""
+export LESS_TERMCAP_ZN=""
+export LESS_TERMCAP_se=$'\E[27m\E(B\E[m'
+export LESS_TERMCAP_ZV=""
+export LESS_TERMCAP_so=$'\E[1m\E[33m\E[44m'
 
 export GROFF_NO_SGR=1         # For Konsole and Gnome-terminal
