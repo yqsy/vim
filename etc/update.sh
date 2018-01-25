99e4cd72a60adaa36339c664f10d3db449897f12
skywind3000 <skywind3000@163.com>
2018-01-26 03:21:11 +0800

    new cheat executable

--- etc/update.sh
+++ etc/update.sh
@@ -7,6 +7,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 [ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2> /dev/null
 [ ! -d "$HOME/.local/etc" ] && mkdir -p "$HOME/.local/etc" 2> /dev/null
 [ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin" 2> /dev/null
+[ ! -d "$HOME/.local/lib" ] && mkdir -p "$HOME/.local/lib" 2> /dev/null
+[ ! -d "$HOME/.local/lib/python" ] && mkdir -p "$HOME/.local/lib/python" 2> /dev/null
 [ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config" 2> /dev/null
 [ ! -d "$HOME/.config/fish" ] && mkdir -p "$HOME/.config/fish" 2> /dev/null
 
@@ -17,6 +19,7 @@ cp $SCRIPTPATH/*.fish "$HOME/.config/fish/"
 cp $SCRIPTPATH/*.zsh "$HOME/.local/etc/"
 
 cp $SCRIPTPATH/../tools/bin/* "$HOME/.local/bin"
+cp $SCRIPTPATH/../lib/* "$HOME/.local/lib/python"
 
 
 
