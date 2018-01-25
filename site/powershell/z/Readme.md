d4df862545bc87ebc56274e116d07968764694b7
skywind3000 <skywind3000@163.com>
2018-01-26 02:35:14 +0800

    update z version, add powershell z

---
+++ site/powershell/z/Readme.md
@@ -0,0 +1,63 @@
+z.ps
+====
+
+This little tool lets you jump directly to your frequently used directories in PowerShell.
+
+![Screenshot](http://i.imgur.com/V2XR0.png)
+
+
+Installation
+------------
+
+ 1. Start PowerShell
+
+ 2. `mkdir '~\My Documents\WindowsPowerShell\Modules'`
+(skip this step if the directory already exists or go somewhere else in your `$Env:PSModulePath`)
+
+ 3. `cd '~\My Documents\WindowsPowerShell\Modules'`
+
+ 4. `git clone https://github.com/JannesMeyer/z.ps.git z`
+
+ 5. Include this in your `~\My Documents\WindowsPowerShell\profile.ps1` (or create a new one if the file doesn't exist yet)
+
+		Import-Module z
+		Set-Alias z Search-NavigationHistory
+		function Prompt {
+			Update-NavigationHistory $pwd.Path
+		}
+
+
+Usage
+-----
+
+Just cd around for a while to let the tool learn your directories.
+
+Then you can use it as follows.
+
+	z asd   # (where 'asd' is part of your desired location's path name)
+
+You can also see a list of all matches before going there.
+
+	z -l asd
+
+Furthermore, you can change the sort algorithm to prioritize recent locations, for example. (The default priorization is a combination of frequency and recency.)
+
+	z -l -s recent asd
+
+The full output of `Get-Help z` looks like this:
+
+	z [[-Patterns] <string>] [-List] [-SortOrder <string> {Default | Recent | Frequent}]  [<CommonParameters>]
+
+
+License
+-------
+
+WTFPL
+
+This program is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public License, Version 2, as published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more details.
+
+
+Planned
+-------
+
+Make module installation easier using http://psget.net/
---
+++ site/powershell/z/z.psm1
@@ -0,0 +1,248 @@
+﻿#
+# PowerShell port of z.sh
+#
+
+$dbfile = "$env:UserProfile\.navdb.csv"
+
+# Tip:
+# You should combine this script with "Push-Location" and "Pop-Location"
+
+# Get-Module -ListAvailable
+# Get-Module
+# Get-Command -Module z
+
+# Execution time:
+# Measure-Command { Update-NavigationHistory $pwd.Path }
+
+function Calculate-FrecencyValue {
+	Param([Int64]$Frequency, [Int64]$LastAccess)
+
+	$now = [DateTime]::UtcNow
+	$last = [DateTime]::FromFileTimeUtc($LastAccess)
+	$factor = switch($last) {
+		{$_.AddHours(1) -gt $now} { 4; break }
+		{$_.AddDays(1) -gt $now}  { 2; break }
+		{$_.AddDays(7) -gt $now}  { 1/2; break }
+		default                   { 1/4 }
+	}
+	return $factor * $Frequency
+}
+
+function MatchAll-Patterns {
+	Param([String]$string, [Array][String]$patterns)
+	
+	# foreach ($pattern in $patterns) {
+	# 	if ($string -inotmatch $pattern) {
+	# 		return $false
+	# 	}
+	# }
+
+	$teststring = $string
+
+	foreach ($pattern in $patterns) {
+		if ($teststring -imatch $pattern) {
+			$index = $teststring.IndexOf($matches[0])
+			$size = $matches[0].length
+			$teststring = $teststring.Substring($index + $size)
+		}	else {
+			return $false
+		}
+	}
+
+	return $true
+}
+
+function Optimize-NavigationHistory {
+	# Make sure that all external hard drives are
+	# plugged in before you continue
+
+	# Import database
+	try {
+		[Array]$navdb = @(Import-Csv $dbfile -Encoding 'Unicode')
+	} catch {
+		$_.Exception.Message
+		return
+	}
+
+	# TODO: Filter out all directories that don't exist
+	# TODO: Filter out the directories that haven't been used in the last 3 months
+	# TODO: Sort database highest rank first?
+	foreach ($item in $navdb) {
+		<#
+		if (!(Test-Path $item.Path)) {
+			# TODO: Delete item
+			continue
+		}
+		#>
+	}
+
+	# Save database
+	try {
+		$navdb | Export-Csv -Path $dbfile -NoTypeInformation -Encoding 'Unicode'
+	} catch {
+		$_.Exception.Message
+	}
+}
+
+function Update-NavigationHistory {
+	#[CmdletBinding()]
+	Param(
+		[parameter(Mandatory=$true)]
+		[String]
+		$Path
+	)
+
+	# Abort if we got $HOME
+	if ($Path -eq $HOME) {
+		return
+	}
+
+	# Import database
+	try {
+		[Array]$navdb = @(Import-Csv $dbfile -Encoding 'Unicode')
+		# TODO: Write a function that handles the database import
+	} catch [System.IO.FileNotFoundException] {
+		[Array]$navdb = @()
+	}
+	
+	# Look for an existing record and update it accordingly
+	$found = $false
+	foreach ($item in $navdb) {
+		# Update Frequency and LastAccess time
+		if ($item.Path -eq $Path) {
+			$found = $true
+			++[Int64]$item.Frequency
+			$item.LastAccess = [DateTime]::Now.ToFileTimeUtc()
+		}
+	}
+	# Nothing found
+	if (!$found) {
+		# Create new object
+		$navdb += [PSCustomObject]@{
+			Path = $Path
+			Frequency = 1
+			LastAccess = [DateTime]::Now.ToFileTimeUtc()
+		}
+		# TODO: Append only one item instead of rewriting the whole file?
+	}
+
+	# Filter out all directories that don't exist
+	$total = 0
+	[Array]$newdb = @()
+
+	foreach ($item in $navdb) {
+		if (Test-path $item.Path) {
+			$newdb += $item
+			$total += $item.Frequency
+		}
+	}
+
+	$navdb = $newdb
+	
+	# Age the complete database if the compound score is above 1000
+	if ($total -gt 1000) {
+		$newdb = @()
+		foreach ($item in $navdb) {
+			[double]$freq = [double]$item.Frequency
+			[Int64]$newf = [math]::floor($freq * 0.9)
+			if ($newf -gt 0) {
+				[Int64]$item.Frequency = [Int64]$newf
+				$newdb += $item
+			}
+		}
+		$navdb = $newdb
+	}
+
+	# Save database
+	try {
+		$navdb | Export-Csv -Path $dbfile -NoTypeInformation -Encoding 'Unicode'
+	} catch {
+		Write-Output $_.Exception.Message
+	}
+}
+
+
+function Search-NavigationHistory {
+	Param(
+		[parameter(ValueFromRemainingArguments=$true, ValueFromPipeline=$true, Position=0)]
+		[String]
+		$Patterns,
+
+		[Switch]
+		$List,
+
+		[ValidateSet('Default', 'Recent', 'Frequent')]
+		[String]
+		$SortOrder='Default'
+	)
+
+	if ([String]::IsNullOrEmpty($Patterns)) {
+		# No search terms given, list everything
+		$List = $true
+		[Array]$PatternList = @()
+	} else {
+		# Convert search terms to Array
+		[Array]$PatternList = $Patterns.Split()
+	}
+
+	# Jump directly for a existing path name
+    if (($PatternList.Count -eq 1) -and (-Not $List)) {
+		if ($PatternList[0] -eq '-') {
+			Pop-Location
+			return
+		}
+		if (Test-path $PatternList[0]) {
+			Set-Location $PatternList[0]
+			return
+		}
+	}
+
+
+	# Import database
+	try {
+		[Array]$navdb = Import-Csv $dbfile -Encoding 'Unicode'
+	} catch [System.IO.FileNotFoundException] {
+		# $_.Exception.Message
+		""
+		return
+	}
+	$navdb | Add-Member -MemberType NoteProperty -Name 'Rank' -Value 0
+
+	# Create a non-fixed-size Array
+	$candidates = New-Object System.Collections.ArrayList
+	# Iterate over every entry in the file
+	foreach ($item in $navdb) {
+		# Ignore this item, if the path doesn't exist
+		if (!(Test-Path $item.Path)) {
+			continue
+		}
+		# Enhance item with Rank
+		$item.Frequency = [Int64]($item.Frequency)
+		$item.LastAccess = [Int64]($item.LastAccess)
+		$item.Rank = switch($SortOrder) {
+			'Frequent' { $item.Frequency }
+			'Recent'   { $item.LastAccess }
+			default    { Calculate-FrecencyValue $item.Frequency $item.LastAccess }
+		}
+		# Match
+		if (MatchAll-Patterns $item.Path $PatternList) {
+			$candidates.Add($item) | Out-Null
+		}
+	}
+	# Nothing found
+	if (!$candidates) {
+		return 'No matches found'
+	}
+
+	if ($List) {
+		# Display the first 20 results
+		$candidates | Sort-Object -Descending Rank | Select-Object Path, Rank -First 20
+	} else {
+		# Change directory
+		$winner = $candidates | Sort-Object -Descending Rank | Select-Object -First 1
+		Set-Location $winner.Path
+	}
+}
+
+
+
